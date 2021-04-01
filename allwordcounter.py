f7a3c0a33854f441e46d8e093e2d6d5b2b4b1a11
Maciej <maciejzadworny@gmail.com>
2021-04-01 19:44:33 +0000

    added features to allwordcounter - deletes "," and turns capitalics to lowercase

--- allwordcounter.py
+++ allwordcounter.py
@@ -5,27 +5,9 @@ The Kombucha mushroom people
 Sitting around all day
 Who can believe you
 Who can believe you
-Let your mother pray (sugar)
-I'm not there all the time you know
-Some people, some people, some people
-Call it insane, yeah they call it insane (sugar)
-I play Russian roulette everyday, a man's sport
-With a bullet called life, yeah, mama, called life(sugar)
-You know that every time I try to go
-Where I really want to be
-It's already where I am
-'Cause I'm already there
-The Kombucha mushroom people
-Sitting around all day
-Who can believe you
-Who can believe you
-Let your mother pray (sugar)
-I got a gun the other day from Sako
-It's cute, small, fits right in my pocket
-Yeah, right in my pocket, (sugar)
-My girl, you know, she lashes out at me sometimes
-And I just fucking kick her
 '''
+words = words.replace(',', '')
+words = words.lower()
 
 word_list = words.split()
 word_count = {}
