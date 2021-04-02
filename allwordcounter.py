47bf431825b6c58d988306037d2e4511f0755398
Maciej <maciejzadworny@gmail.com>
2021-04-02 19:44:57 +0000

    Added features

--- allwordcounter.py
+++ allwordcounter.py
@@ -1,12 +1,32 @@
 import operator
 
 words = '''
-The Kombucha mushroom people
-Sitting around all day
-Who can believe you
-Who can believe you
+I've been watchin' you for some time
+Can't stop starin' at those ocean eyes
+Burning cities and napalm skies
+Fifteen flares inside those ocean eyes
+Your ocean eyes
+No fair
+You really know how to make me cry
+When you gimme those ocean eyes
+I'm scared
+I've never fallen from quite this high
+Fallin' into your ocean eyes
+Those ocean eyes
+I've been walkin' through a world gone blind
+Can't stop thinkin' of your diamond mind
+Careful creature made friends with time
+He left her lonely with a diamond mind
+And those ocean eyes
+No fair
+You really know how to make me cry
+When you gimme those ocean eyes
+I'm scared
+I've never fallen from quite this high…
 '''
-words = words.replace(',', '')
+remove_these = [',', '.', '…']
+for thing in remove_these:
+  words = words.replace(thing,"")
 words = words.lower()
 
 word_list = words.split()
@@ -19,6 +39,10 @@ for word in word_list:
     word_count[word] = 1
 
 sorted_count = sorted(word_count.items(), key=operator.itemgetter(1),reverse=True)
-print(sorted_count)
+
+print(f'There are {len(sorted_count)} unique words in this.')
+
+for word_tuple in sorted_count:
+  print(f'{word_tuple[0]} - {word_tuple[1]}')
 
 
